2efff05d5bafe848358962a8ae6898c346b901e5
Denis <desivaev@ozon.ru>
2023-10-15 13:48:03 +0300

    pass 3st stage

--- app/server.go
+++ app/server.go
@@ -4,9 +4,23 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 var responseOk []byte = []byte("HTTP/1.1 200 OK\r\n\r\n")
+
+var responseNotOk []byte = []byte("HTTP/1.1 404 Not Found\r\n\r\n")
+
+func getPath(buf []byte) string {
+	str := string(buf)
+	rows := strings.Split(str, "\r\n")
+
+	firstRowContent := strings.Split(rows[0], " ")
+
+	path := firstRowContent[0]
+
+	return path
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -23,11 +37,26 @@ func main() {
 	}
 	defer conn.Close()
 
-	conn.Read(nil)
+	buffer := make([]byte, 1024)
 
-	_, err = conn.Write(responseOk)
+	_, err = conn.Read(buffer)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("my programm read:%v\n", err)
+	}
+
+	path := getPath(buffer)
+
+	var response []byte
+
+	if path == "/" {
+		response = responseOk
+	} else {
+		response = responseNotOk
+	}
+
+	_, err = conn.Write(response)
+	if err != nil {
+		fmt.Printf("my programm write:%v\n", err)
 	}
 
 }
