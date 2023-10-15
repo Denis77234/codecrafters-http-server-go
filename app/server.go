f34e1835ff2f5e5fd4b88787c34bb83622a3b04c
Denis <desivaev@ozon.ru>
2023-10-15 19:15:39 +0300

    pass 4st stage

--- app/server.go
+++ app/server.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+	"github.com/codecrafters-io/http-server-starter-go/internal/server"
 )
 
 var responseOk []byte = []byte("HTTP/1.1 200 OK\r\n\r\n")
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
-	}
-	defer l.Close()
 
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	defer conn.Close()
+	s := server.Server{}
 
-	buffer := make([]byte, 1024)
-
-	_, err = conn.Read(buffer)
-	if err != nil {
-		fmt.Printf("my programm read:%v\n", err)
-	}
-
-	_, err = conn.Write(responseOk)
-	if err != nil {
-		fmt.Printf("my programm write:%v\n", err)
-	}
+	s.Start()
 
 }
