e219d15f68a597f9c06175102037966f20fd378c
Denis <desivaev@ozon.ru>
2023-10-17 11:33:56 +0300

    pass 7st stage

--- app/server.go
+++ app/server.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"errors"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/codecrafters-io/http-server-starter-go/internal/server"
@@ -8,7 +13,29 @@ import (
 
 func main() {
 
+	dir := flag.String("directory", "", "file directory")
+	flag.Parse()
+
 	serv := server.New("tcp", "0.0.0.0:4221")
+
+	serv.AddHandler("/file", func(req server.Request, w server.ResponseWriter) {
+		filename := strings.TrimPrefix(req.URL.Path, "/file/")
+		path := filepath.Join(*dir, filename)
+		if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+			w.WriteStatus(server.STATUS_404_NOTFOUND)
+			return
+		}
+		file, err := os.ReadFile(path)
+		if err != nil {
+			log.Println(err)
+			w.WriteStatus(server.STATUS_404_NOTFOUND)
+			return
+		}
+
+		w.WriteStatus(server.STATUS_200_OK)
+		w.WriteContentType("application/octet-stream")
+		w.WriteBody(file)
+	})
 
 	serv.AddHandler("/user-agent", func(req server.Request, w server.ResponseWriter) {
 		agent := req.Header["User-Agent"]
