16fa596bd894d963f158015568a491567f404207
Denis <desivaev@ozon.ru>
2023-10-22 12:40:30 +0300

    pass 7st stage

--- app/server.go
+++ app/server.go
@@ -40,10 +40,6 @@ func main() {
 		if req.Method == server.METHOD_POST {
 			filename := strings.TrimPrefix(req.URL, "/files/")
 			path := filepath.Join(*dir, filename)
-			if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
-				w.WriteStatus(server.STATUS_404_NOTFOUND)
-				return
-			}
 
 			os.WriteFile(path, req.Body, 0644)
 			w.WriteStatus(server.STATUS_200_OK)
